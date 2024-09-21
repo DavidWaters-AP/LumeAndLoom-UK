1ae9ed16c1ffb77d9d810661a4082d987e496de7
AlternatePro <david.waters@alternate.pro>
2024-09-21 16:28:56 +1200

    w

--- woonuxt_base/app/pages/index.vue
+++ woonuxt_base/app/pages/index.vue
@@ -2,7 +2,7 @@
 import { ProductsOrderByEnum } from '#woo';
 const { siteName, description, shortDescription, siteImage } = useAppConfig();
 
-const { data } = await useAsyncGql('getProductCategories', { first: 6 });
+const { data } = await useAsyncGql('getProductCategories', { first: 5 });
 const productCategories = data.value?.productCategories?.nodes || [];
 
 const { data: productData } = await useAsyncGql('getProducts', { first: 8, orderby: ProductsOrderByEnum.POPULARITY });
@@ -30,28 +30,19 @@ useSeoMeta({
       <ProductRow :products="popularProducts" class="grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8" />
     </section>
 
-    <div class="container flex flex-wrap items-center justify-center my-16 text-center gap-x-8 gap-y-4 brand lg:justify-between">
-      <img src="/images/logoipsum-211.svg" alt="Brand 1" width="132" height="35" />
-      <img src="/images/logoipsum-221.svg" alt="Brand 2" width="119" height="30" />
-      <img src="/images/logoipsum-225.svg" alt="Brand 3" width="49" height="48" />
-      <img src="/images/logoipsum-280.svg" alt="Brand 4" width="78" height="30" />
-      <img src="/images/logoipsum-284.svg" alt="Brand 5" width="70" height="44" />
-      <img src="/images/logoipsum-215.svg" alt="Brand 6" width="132" height="40" />
-    </div>
-
     <section class="container my-16">
       <div class="flex items-end justify-between">
         <h2 class="text-lg font-semibold md:text-2xl">{{ $t('messages.shop.shopByCategory') }}</h2>
         <NuxtLink class="text-primary" to="/categories">{{ $t('messages.general.viewAll') }}</NuxtLink>
       </div>
-      <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-6">
+      <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-5">
         <CategoryCard v-for="(category, i) in productCategories" :key="i" class="w-full" :node="category" />
       </div>
     </section>
 
     <section class="container grid gap-4 my-24 md:grid-cols-2 lg:grid-cols-4">
       <div class="flex items-center gap-8 p-8 bg-white rounded-lg">
-        <img src="/icons/box.svg" width="60" height="60" alt="Free Shipping" loading="lazy" />
+        <img src="/icons/box.svg" class="text-red-500" width="60" height="60" alt="Free Shipping" loading="lazy" />
         <div>
           <h3 class="text-xl font-semibold">Free Shipping</h3>
           <p class="text-sm">Free shipping on order over €50</p>
