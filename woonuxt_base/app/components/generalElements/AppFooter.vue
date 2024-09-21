a72677884a2f361ff46a6de90da08ff891bae8e1
AlternatePro <david.waters@alternate.pro>
2024-09-21 20:16:29 +1200

    w

--- woonuxt_base/app/components/generalElements/AppFooter.vue
+++ woonuxt_base/app/components/generalElements/AppFooter.vue
@@ -14,7 +14,9 @@ const { wishlistLink } = useAuth();
       <div class="w-3/7 lg:w-auto">
         <div class="mb-1 font-semibold">Information</div>
         <div class="text-sm">
-          <a class="py-1.5 block" href="https://github.com/scottyzen/woonuxt?tab=readme-ov-file#next-generation-front-end-for-woocommerce" target="_blank">About</a>
+          <a class="py-1.5 block" href="https://github.com/scottyzen/woonuxt?tab=readme-ov-file#next-generation-front-end-for-woocommerce" target="_blank"
+            >About</a
+          >
           <a href="/" class="py-1.5 block">Careers</a>
           <a href="/" class="py-1.5 block">Press</a>
           <a href="https://woonuxt.com/faq" class="py-1.5 block" rel="noreferrer" target="_blank">FAQ's</a>
@@ -50,10 +52,7 @@ const { wishlistLink } = useAuth();
     </div>
     <div class="container border-t flex items-center justify-center mb-4">
       <div class="copywrite">
-        <p class="py-4 text-xs text-center">
-          <a href="https://woonuxt.com" :title="`WooNuxt v${wooNuxtVersionInfo}`">{{ `WooNuxt v${wooNuxtVersionInfo}` }}</a> - by
-          <a href="https://scottyzen.com" title="Scott Kennedy - Web Developer" target="_blank">Scott Kennedy</a>
-        </p>
+        <p class="py-4 text-xs text-center">© 2024 Lume & Loom -|- Website By <a href="https://www.alternate.pro">Alternate</a></p>
       </div>
       <SocialIcons class="ml-auto" />
     </div>
