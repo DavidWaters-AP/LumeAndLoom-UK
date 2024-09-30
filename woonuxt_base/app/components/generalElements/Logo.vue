5768d8e249d5675bf5ea4a0391bb714552541c4f
AlternatePro <david.waters@alternate.pro>
2024-09-30 15:11:23 +1300

    fixed logo path issue

--- woonuxt_base/app/components/generalElements/Logo.vue
+++ woonuxt_base/app/components/generalElements/Logo.vue
@@ -1,18 +1,7 @@
-<script setup lang="ts">
-const { siteName } = useAppConfig();
-const runtimeConfig = useRuntimeConfig();
-const img = useImage();
-
-const logoUrl = 'images/lumeandloom_logo-dark.svg';
-const faviconUrl = '/logo.svg';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <NuxtLink to="/">
-    <img v-if="logoUrl" :src="logoUrl" alt="Logo" class="object-contain h-4" />
-    <div v-else class="flex items-center gap-2 text-lg font-bold">
-      <img :src="faviconUrl" alt="Logo" width="32" height="32" />
-      <span>{{ siteName }}</span>
-    </div>
+    <img src="/images/lumeandloom_logo-dark.svg" alt="Lume & Loom Logo" class="object-contain h-4" />
   </NuxtLink>
 </template>
